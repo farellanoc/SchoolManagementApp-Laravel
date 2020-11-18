a896936ec533395fa99ab1ae58a179e69ba91cee
cristianvia <cristianviar@gmail.com>
2020-11-18 17:43:45 +0100

    add index and profile routes to web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/profile', function () {
+    return view('profile');
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {

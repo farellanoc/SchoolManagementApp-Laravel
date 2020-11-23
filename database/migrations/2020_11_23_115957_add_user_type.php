c4cc5947501d021752e78d16e22d35e1641aa82c
cristianvia <cristianviar@gmail.com>
2020-11-23 21:46:59 +0100

    Add user_type migration into users

---
+++ database/migrations/2020_11_23_115957_add_user_type.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserType extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+
+            $table->smallInteger('user_type');
+            //php artisan migrate --path=/database/migrations/2020_11_23_115957_add_user_type.php
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}

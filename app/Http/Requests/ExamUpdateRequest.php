27aa71f8a4dce074cba855e0da021409d422cc33
cristianvia <cristianviar@gmail.com>
2020-12-03 18:12:10 +0100

    Create Requests for store and update

---
+++ app/Http/Requests/ExamUpdateRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ExamUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'string'],
+            'mark' => ['required', 'integer'],
+            'time_start' => ['required'],
+            'time_end' => ['required'],
+            'user_id' => ['required', 'integer', 'exists:users,id'],
+            'subject_id' => ['required', 'integer', 'exists:subjects,id'],
+        ];
+    }
+}

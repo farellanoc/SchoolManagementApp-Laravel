b9afca16d1361dbf14623e1a456f80dead6bcc5d
cristianvia <cristianviar@gmail.com>
2020-12-04 20:14:55 +0100

    Add svg icons to submenu options

--- resources/views/menu/app.blade.php
+++ resources/views/menu/app.blade.php
@@ -1,37 +1,60 @@
 <div class="submenu-options">
     @can('canAccessCalendar')
-        <div class="item-option margin-right-15 cursor-pointer">
-                <a href="{{route('calendar.index')}}">Calendario</a>
-        </div>
+    <div class="item-option margin-right-15 cursor-pointer">
+        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-calendar-plus-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4V.5zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2zM8.5 8.5a.5.5 0 0 0-1 0V10H6a.5.5 0 0 0 0 1h1.5v1.5a.5.5 0 0 0 1 0V11H10a.5.5 0 0 0 0-1H8.5V8.5z" />
+        </svg>
+        <a href="{{route('calendar.index')}}">Calendario</a>
+    </div>
     @endcan
     @can('canAdminSubjects')
-        <div class="item-option margin-right-15 cursor-pointer">
-            <a href="{{route('subject.index')}}">Asignaturas</a>
-        </div>
+    <div class="item-option margin-right-15 cursor-pointer">
+
+        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
+        </svg>
+        <a href="{{route('subject.index')}}">Asignaturas</a>
+    </div>
     @endcan
     @can('canAdminUsers')
-        <div class="item-option margin-right-15 cursor-pointer">
-            <a href="{{route('user.index')}}">Usuarios</a>
-        </div>
+    <div class="item-option margin-right-15 cursor-pointer">
+        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-people-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z" />
+        </svg>
+        <a href="{{route('user.index')}}">Usuarios</a>
+    </div>
     @endcan
     @can('canAdminEnrollments')
-        <div class="item-option margin-right-15 cursor-pointer">
-            <a href="{{route('enrollment.index')}}">Matrículas</a>
-        </div>
+    <div class="item-option margin-right-15 cursor-pointer">
+        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-archive-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M12.643 15C13.979 15 15 13.845 15 12.5V5H1v7.5C1 13.845 2.021 15 3.357 15h9.286zM5.5 7a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zM.8 1a.8.8 0 0 0-.8.8V3a.8.8 0 0 0 .8.8h14.4A.8.8 0 0 0 16 3V1.8a.8.8 0 0 0-.8-.8H.8z" />
+        </svg>
+        <a href="{{route('enrollment.index')}}">Matrículas</a>
+    </div>
     @endcan
     @can('canShowCourses')
-        <div class="item-option margin-right-15 cursor-pointer">
-            <a href="{{route('course.index')}}">Cursos</a>
-        </div>
+    <div class="item-option margin-right-15 cursor-pointer">
+        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-book-half" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M8.5 2.687v9.746c.935-.53 2.12-.603 3.213-.493 1.18.12 2.37.461 3.287.811V2.828c-.885-.37-2.154-.769-3.388-.893-1.33-.134-2.458.063-3.112.752zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z" />
+        </svg>
+        <a href="{{route('course.index')}}">Cursos</a>
+    </div>
     @endcan
     @can('canAdminExam')
-        <div class="item-option margin-right-15 cursor-pointer">
-            <a href="{{route('exam.index')}}">Exámenes</a>
-        </div>
+    <div class="item-option margin-right-15 cursor-pointer">
+        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-award-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path d="M8 0l1.669.864 1.858.282.842 1.68 1.337 1.32L13.4 6l.306 1.854-1.337 1.32-.842 1.68-1.858.282L8 12l-1.669-.864-1.858-.282-.842-1.68-1.337-1.32L2.6 6l-.306-1.854 1.337-1.32.842-1.68L6.331.864 8 0z" />
+            <path d="M4 11.794V16l4-1 4 1v-4.206l-2.018.306L8 13.126 6.018 12.1 4 11.794z" />
+        </svg>
+        <a href="{{route('exam.index')}}">Exámenes</a>
+    </div>
     @endcan
     @can('canAdminWork')
-        <div class="item-option margin-right-15 cursor-pointer">
-            <a href="{{route('work.index')}}">Trabajos</a>
-        </div>
+    <div class="item-option margin-right-15 cursor-pointer">
+        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-file-code-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM6.646 5.646a.5.5 0 1 1 .708.708L5.707 8l1.647 1.646a.5.5 0 0 1-.708.708l-2-2a.5.5 0 0 1 0-.708l2-2zm2.708 0a.5.5 0 1 0-.708.708L10.293 8 8.646 9.646a.5.5 0 0 0 .708.708l2-2a.5.5 0 0 0 0-.708l-2-2z" />
+        </svg>
+        <a href="{{route('work.index')}}">Trabajos</a>
+    </div>
     @endcan
 </div>
